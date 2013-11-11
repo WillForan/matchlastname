4dd95c242b8825bbf9dc82655cc2c3a7a040b808
Will Foran <willforan@gmail.com>
2013-11-11 16:57:11 -0500

    add more comments

--- getNameAtAddress.js
+++ getNameAtAddress.js
@@ -1,5 +1,13 @@
+// file to check address against last name
+// https://phantomjs.googlecode.com/files/phantomjs-1.9.2-windows.zip
 // https://github.com/ariya/phantomjs/wiki/Quick-Start
 // http://stackoverflow.com/questions/9246438/how-to-submit-a-form-using-phantomjs
+// http://stackoverflow.com/questions/10865849/phantomjs-javascript-read-a-local-file-line-by-line
+//
+
+/* do stuff locally */
+
+/* do stuff on the web */
 var page=require('webpage').create();
 
 page.open('',submitValues) // open the page
