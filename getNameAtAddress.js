2845706a785939159ec1d970b370e4bbd8a62056
Will Foran <willforan@gmail.com>
2013-11-11 16:44:03 -0500

    init

---
+++ getNameAtAddress.js
@@ -0,0 +1,9 @@
+// https://github.com/ariya/phantomjs/wiki/Quick-Start
+// http://stackoverflow.com/questions/9246438/how-to-submit-a-form-using-phantomjs
+var page=require('webpage').create();
+
+page.open('',submitValues) // open the page
+
+function submitValues(){
+ //fill out form
+}
