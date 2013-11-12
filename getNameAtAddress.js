045ae1371ccba7573282e13d7013d467d8ddca36
Will Foran <willforan@gmail.com>
2013-11-12 13:31:39 -0500

    submit and parse, now with casperjs

--- getNameAtAddress.js
+++ getNameAtAddress.js
@@ -1,10 +1,11 @@
+// run: casperjs getNameAtAddress.js
+// casperjs getNameAtAddress.js | tee -a stillthere.txt
 //
 // TODOS
-//  1. get submit to work
-//  2. pull last name from new pages' dome
-//     - follow a redirect
-//  3. compare to expected
-//  4. write out all who match
+//  1.DONE get submit to work
+//  2.DONE pull last name from new page's dom
+//  3.DONE compare to expected
+//  4. write out all who match [ERROR in casper?]
 //
 //
 // file to check address against last name
@@ -13,27 +14,83 @@
 // http://stackoverflow.com/questions/9246438/how-to-submit-a-form-using-phantomjs
 // http://stackoverflow.com/questions/10865849/phantomjs-javascript-read-a-local-file-line-by-line
 //
+// http://docs.casperjs.org/en/latest/modules/casper.html#fill
+
+'use strict';
 
 /* do stuff locally */
-var fs = require('fs');
-var filedata = fs.read('namesAndAddress.txt');     // read the file into a single string
-var arrdata = filedata.split(/[\r\n]/); // split the string on newline and store in array
 
-// for arrdata.lenght -> split by \t into 2d array (address and lastname)
+var system=require('system'), //not needed
+    fs = require('fs');
+var filedata = fs.read('namesAndAddress.txt'),     // read the file into a single string
+    arrdata = filedata.split(/[\r\n]/); // split the string on newline and store in array
+
+//var outfile = open('stillthere.txt','w');
+
+var data=[];
+//StudentLastName	StudentFirstName	retriveDate	DOB	AgeMonths	AgeYears	ZipCode	StreetName	StreetNumber	StreetNumberHalf	StreetDirection	StreetSuffix	Apt.Number	City	State	PrimaryHomeAddress
+for(var idx=0; idx<arrdata.length;idx++){
+  data[idx]=arrdata[idx].split(/\t/);
+}
 
 
 /* do stuff on the web for each name*/
-//TODO
-// write + put in for loop
-var page=require('webpage').create();
-function submitValues(){
- //fill out form
- //submit
-}
-//TODO, how to read submited data
+//TODO: for loop
+// values at [0][*] are titles
+//for(var i=1;i<3;i++){
+var  casper = require('casper').create();
+casper.start();
+var i=1;
+casper.repeat(arrdata.length-2,function(){ this.wait(3000,function(){
 
-page.open('http://www2.county.allegheny.pa.us/RealEstate/Search.aspx',submitValues) // open the page
+  var name = data[i][0];
+  var st   = data[i][7];
+  var num  = data[i][8];
+  //console.log(i,name,st,num)
+  
+  /*queue: load the page*/
+  casper.thenOpen('http://www2.county.allegheny.pa.us/RealEstate/Search.aspx', function() {
+    //this.capture('open.png')
+    //console.log('form exists?', casper.exists('form#Form10'))
+    this.fill(
+     'form#Form10',
+     { 'txtStreetName': st,
+       'txtStreetNum':  num },
+      false);
+    //this.capture('filled.png')
+    this.click('#btnSearch');
+    document.querySelector('input#btnSearch').submit();
+  });
+  
+  /*queue: parse the results*/
+  casper.then(function() {
+    //this.capture('final.png');
+    //console.log('label exists?', casper.exists('span#BasicInfo1_lblOwner'))
+    //console.log('clicked ok, new loc ' + this.getCurrentUrl())
+    var url=this.getCurrentUrl();
+    var label=this.evaluate(function(){return document.querySelector('span#BasicInfo1_lblOwner').textContent.trim()})
+    var regname=new RegExp(name,'i');
+    var match=regname.test(label);
+    //console.log(url)
+    //this.echo(i +': ' +name + ' match web ' + label + '? ' + match,'INFO')
+    if(match){
+     //TODO: write to file
+     //outfile.writeLine( arrdata[i].concat([label,url]).join("\t")  )
+     var content=arrdata[i] + "\t" + label + "\t" + url //+"\n" ;
+     //console.log('writting ' + content)
+     //write('stillthere.txt',content,'a')
+     console.log(content)
+    
+    }
+    else {
+     //this.echo('not writting')
+     //this.capture('error.png')
+    }
 
-
-
-
+  })
+  
+  i++;
+})})
+/*execute*/
+casper.run()
+//close(outfile)
